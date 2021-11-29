8a0fc0e70676e0d19523395c59d313b407407d24
mahary <maharyfenitra@gmail.com>
2021-11-29 12:59:28 +0000

    Second commit with mui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import {Typography,AppBar,Card,CardActions,CardContent,CardMedia,CssBaseline,Grid,Toolbar,Container} from '@mui/material/';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world!</p>
-      </header>
-    </div>
+    <>
+      <AppBar position="relative">
+        <Toolbar>
+            <AccessAlarmIcon/>
+            <Typography variant="h6 ">
+            Hello React!
+            </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+      <Container maxWidth="md">
+      
+      <Grid container spacing={4}>
+      <Grid item style={{margin:"10px"}}>
+          <FormGroup>
+              <FormControlLabel control={<Checkbox defaultChecked />} label="Task 1" />
+              <FormControlLabel control={<Checkbox defaultChecked />} label="Task 2" />
+        </FormGroup>
+      </Grid>
+      </Grid>
+      </Container>
+      
+      </main>
+        
+      
+    </>
   );
 }
 
