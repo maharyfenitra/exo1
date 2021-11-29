42f82098651b710d3c94d1cf6dc7b3e50efdd088
mahary <maharyfenitra@gmail.com>
2021-11-29 16:06:27 +0000

    Last commit for today

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import {Typography,AppBar,Card,CardActions,CardContent,CardMedia,CssBaseline,Gri
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import {useState} from 'react';
 
 function App() {
+  const [newTask,setNewTask] = useState(""); 
+  const [taskList,setTaskList]=useState(["Task 1","Task 2"]);
+  const handleClick = ()=>{
+    setTaskList([...taskList,newTask]);
+  }
+  
   return (
     <>
       <AppBar position="relative">
@@ -22,8 +30,17 @@ function App() {
       <Grid container spacing={4}>
       <Grid item style={{margin:"10px"}}>
           <FormGroup>
-              <FormControlLabel control={<Checkbox defaultChecked />} label="Task 1" />
-              <FormControlLabel control={<Checkbox defaultChecked />} label="Task 2" />
+              {taskList.map((task)=><FormControlLabel control={<Checkbox defaultChecked />} label={task} />)}
+              
+              <TextField value={newTask} onChange={(e)=>{setNewTask(e.target.value);}}
+                  required
+                  id="outlined-required"
+                  label="New Task"
+                  defaultValue="New task"
+              />
+              <Button size="small" variant="contained" style={{marginTop:"20px"}} onClick={()=> handleClick()} color="primary">
+                    Save task
+              </Button>
         </FormGroup>
       </Grid>
       </Grid>
